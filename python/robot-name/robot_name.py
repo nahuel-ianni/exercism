6e856c8fc02b53b7cc0cb06be92f80c92f2f3ae0
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-04 16:58:05 +0100

    Solved exercise: Robot-Name

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,3 +1,40 @@
+import random
+import string
+
+
 class Robot:
+    namesTaken = set()
+
     def __init__(self):
-        pass
+        self.name = ""
+        self.reset()
+
+    
+    def reset(self):
+        formerName = self.name
+
+        while self.name in Robot.namesTaken or not self.name:
+            self.name = self.getRandomName()
+
+        Robot.namesTaken.discard(formerName)
+        Robot.namesTaken.add(self.name)
+
+
+    def getRandomName(self):
+        characters = self.getRandomLetters()
+        number = self.getRandomNumber()        
+
+        return f"{characters}{number}"
+
+    
+    def getRandomLetters(self):
+        letters = random.choices(string.ascii_uppercase, k=2)
+
+        return "".join(letters)
+
+
+    def getRandomNumber(self):
+        number = random.randint(0, 999)
+        number = '{:03}'.format(number)
+        
+        return number
