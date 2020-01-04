79b7868d734eee92583033145fe5ca7e44955374
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-04 18:42:46 +0100

    Changed solution with a helper method approach

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,5 +1,9 @@
 def leap_year(year):
-    check1 = (year % 4) == 0 and (year % 100) != 0
-    check2 = (year % 100) == 0 and (year % 400) == 0
+    check1 = is_divisible(year, 4) and not is_divisible(year, 100)
+    check2 = is_divisible(year, 100) and is_divisible(year, 400)
 
     return check1 or check2
+
+
+def is_divisible(a: int, b: int) -> bool:
+    return (a % b) == 0
