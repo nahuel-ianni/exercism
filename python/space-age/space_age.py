d467a4a34f3246ca0242f5bab52cb6238bcff71a
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-04 21:34:44 +0100

    Solved exercise "Space age"

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,53 +1,45 @@
 class SpaceAge:
     def __init__(self, seconds):
         self.seconds = seconds
-    
-
-    def round(self, number):
-        return round(number, 2)
 
 
     def on_earth(self):
-        return self.round(self.seconds / 31557600)
+        return self.calculate_years(1)
 
 
     def on_mercury(self):
-        localSecondsPerYear = 0.2408467 * 31557600
-        
-        return self.round(self.seconds / localSecondsPerYear)
+        return self.calculate_years(0.2408467)
 
 
     def on_venus(self):
-        localSecondsPerYear = 0.61519726 * 31557600
-
-        return self.round(self.seconds / localSecondsPerYear)
+         return self.calculate_years(0.61519726)
 
 
     def on_mars(self):
-        localSecondsPerYear = 1.8808158 * 31557600
-
-        return self.round(self.seconds / localSecondsPerYear)
+        return self.calculate_years(1.8808158)
 
 
     def on_jupiter(self):
-        localSecondsPerYear = 11.862615 * 31557600
-
-        return self.round(self.seconds / localSecondsPerYear)
+        return self.calculate_years(11.862615)
 
 
     def on_saturn(self):
-        localSecondsPerYear = 29.447498 * 31557600
-
-        return self.round(self.seconds / localSecondsPerYear)
+        return self.calculate_years(29.447498)
 
 
     def on_uranus(self):
-        localSecondsPerYear = 84.016846 * 31557600
-
-        return self.round(self.seconds / localSecondsPerYear)
+        return self.calculate_years(84.016846)
 
 
     def on_neptune(self):
-        localSecondsPerYear = 164.79132 * 31557600
+        return self.calculate_years(164.79132)
 
-        return self.round(self.seconds / localSecondsPerYear)
+    
+    def orbital_period_to_seconds(self, orbital_period):
+        return orbital_period * 31557600
+
+
+    def calculate_years(self, orbital_period):
+        planetary_seconds = self.orbital_period_to_seconds(orbital_period)
+        
+        return round(self.seconds / planetary_seconds, 2)
