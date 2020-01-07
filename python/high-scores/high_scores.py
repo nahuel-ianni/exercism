af768a1e2502eba27dd2e8d1db254bcbedeadb66
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-07 21:39:08 +0100

    Solved "High scores" (fail on uncovered cases)

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,10 +1,13 @@
 def latest(scores):
-    pass
+    return scores[-1]
 
 
 def personal_best(scores):
-    pass
+    return max(scores)
 
 
 def personal_top_three(scores):
-    pass
+    temp_scores = scores.copy()
+    temp_scores.sort(reverse=True)
+
+    return temp_scores[0:3]
