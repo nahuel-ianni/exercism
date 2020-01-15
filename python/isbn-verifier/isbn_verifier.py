a2e8f602e2593a32ec2e5de0865b2938c7d18da9
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-15 11:09:46 +0100

    ISBN: Class formatting (empty line)

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,5 +1,6 @@
 import re as regex
 
+
 def is_valid(isbn):
     pattern = "^([0-9]-*)([0-9]{3}-*)([0-9]{5}-*)([0-9]|X)$"
     formula = "(n0 * 10 + n1 * 9 + n2 * 8 + n3 * 7 + n4 * 6 + n5 * 5 + n6 * 4 + n7 * 3 + n8 * 2 + n9 * 1) % 11 == 0"
