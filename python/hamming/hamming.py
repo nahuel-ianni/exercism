0f8d9a370256d5a31ad7a77d889ba9fe66054497
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-17 19:58:59 +0100

    Hamming: Solved exercise

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,6 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Sequences must be of equal length.")
+
+    zipped_strands = zip(strand_a, strand_b)
+    return sum(n1 != n2 for n1, n2 in zipped_strands)
