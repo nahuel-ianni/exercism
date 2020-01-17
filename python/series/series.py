8ddab7e2b06ced6c61e981fe6b40cdb160aada0d
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-17 22:13:54 +0100

    Series: Solved exercise

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,5 @@
 def slices(series, length):
-    pass
+    if length > len(series) or length < 1:
+        raise ValueError("The length of the subset cannot be computed.")
+
+    return [series[i:i + length] for i in range(len(series)) if i + length <= len(series)]
