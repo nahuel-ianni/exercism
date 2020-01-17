c8448e4687cba4df0e051adbd07aa540af90f13a
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-17 20:32:53 +0100

    CollatzConjecture: Solved exercise

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,12 @@
-def steps(number):
-    pass
+def steps(number, step = 0):
+    guard_value(number)
+
+    if number == 1:
+        return step
+
+    return steps(number / 2 if number % 2 == 0 else 3 * number + 1, step + 1)
+    
+
+def guard_value(number):
+    if number < 1:
+        raise ValueError("Value cannot be lower than 1")
