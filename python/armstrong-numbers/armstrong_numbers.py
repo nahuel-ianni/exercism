aef69f9a5d6ec9e7bd78771241ceefd14b3b44ff
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-18 22:59:19 +0100

    ArmstrongNumbers: Removed empty line

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -3,7 +3,6 @@ def is_armstrong_number(number):
     Used a list(map(...)) on the exercise instead of str(number) to create a sequence as this avoids having to cast
     'd' to an integer when performing the sum, which makes the function faster from ~0.13 to ~0.09, as explained here:
     https://stackoverflow.com/questions/13905936/converting-integer-to-digit-list
-
     """
 
     sequence = list(map(int, str(number)))
