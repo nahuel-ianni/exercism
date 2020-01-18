3c90166c83428b8b64900f0e8d29694c6d7a5dd5
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-18 22:24:52 +0100

    RnaTranscription: Added comment

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -10,6 +10,9 @@ def to_rna(dna_strand):
 
 
 def to_dna(rna_strand):
+    """ This method was added by me to keep two different solutions in the same place for future references. """
+    """ New tests were added for the method. """
+    
     nucleotide_complements = str.maketrans("CGAU", "GCTA")
     
     return rna_strand.translate(nucleotide_complements)
