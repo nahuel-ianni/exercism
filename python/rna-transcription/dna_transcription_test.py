2af81d07587f97bd6270c11c9a28cc6f8e30ab6b
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-18 22:22:03 +0100

    RnaTransription: Added a new solution, kept both

---
+++ python/rna-transcription/dna_transcription_test.py
@@ -0,0 +1,29 @@
+import unittest
+
+from rna_transcription import to_dna
+
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.3.0
+
+
+class DnaTranscriptionTest(unittest.TestCase):
+    def test_empty_dna_sequence(self):
+        self.assertEqual(to_dna(""), "")
+
+    def test_dna_complement_of_guanine_is_cytosine(self):
+        self.assertEqual(to_dna("G"), "C")
+
+    def test_dna_complement_of_cytosine_is_guanine(self):
+        self.assertEqual(to_dna("C"), "G")
+
+    def test_dna_complement_of_adenine_is_thymine(self):
+        self.assertEqual(to_dna("A"), "T")
+
+    def test_dna_complement_of_uracil_is_adenine(self):
+        self.assertEqual(to_dna("U"), "A")
+
+    def test_dna_complement(self):
+        self.assertEqual(to_dna("UGCACCAGAAUU"), "ACGTGGTCTTAA")
+
+
+if __name__ == "__main__":
+    unittest.main()
--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -7,3 +7,9 @@ def to_rna(dna_strand):
     }
 
     return "".join([nucleotide_complements.get(n) for n in dna_strand])
+
+
+def to_dna(rna_strand):
+    nucleotide_complements = str.maketrans("CGAU", "GCTA")
+    
+    return rna_strand.translate(nucleotide_complements)
