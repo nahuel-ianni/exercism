1574e97f4f5cefdb3a7f1e3d17e5a41fa8d0f28e
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-18 14:27:35 +0100

    PerfectNumbers: Solved exercise

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,2 +1,14 @@
 def classify(number):
-    pass
+    if number < 1:
+        raise ValueError("Number must be bigger than 0.")
+
+    aliquot_sum = sum([i for i in range(1, number // 2 + 1) if number % i == 0])
+
+    if aliquot_sum == number:
+        return "perfect"
+
+    elif aliquot_sum > number:
+        return "abundant"
+        
+    else:
+        return "deficient"
