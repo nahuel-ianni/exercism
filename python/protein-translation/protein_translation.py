9566168e9c3dabbf37759f6cb67a96604328af89
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-18 23:33:47 +0100

    ProteinTranslation: Solved exercise

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,28 @@
+amino_acid = {
+    "AUG": "Methionine",
+    "UUU": "Phenylalanine",
+    "UUC": "Phenylalanine",
+    "UUA": "Leucine",
+    "UUG": "Leucine",
+    "UCU": "Serine",
+    "UCC": "Serine",
+    "UCA": "Serine",
+    "UCG": "Serine",
+    "UAU": "Tyrosine",
+    "UAC": "Tyrosine",
+    "UGU": "Cysteine",
+    "UGC": "Cysteine",
+    "UGG": "Tryptophan",
+    "UAA": "STOP",
+    "UAG": "STOP",
+    "UGA": "STOP",
+}
+
 def proteins(strand):
-    pass
+    if len(strand) % 3 != 0:
+        raise ValueError("Unsupported operation: The strand info doesn't allow creating proper length codons (3 char long).")
+
+    codons = [strand[i:i+3] for i in range(0, len(strand), 3)]
+    amino_acids = [amino_acid.get(c) for c in codons]
+
+    return amino_acids if "STOP" not in amino_acids else amino_acids[:amino_acids.index("STOP")]
