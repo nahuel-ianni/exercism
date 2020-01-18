9a05a2cc3994e2bc508203ecc6519ae155385801
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-18 22:57:25 +0100

    ArmstrongNumbers: Solved exercise

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,12 @@
 def is_armstrong_number(number):
-    pass
+    """ 
+    Used a list(map(...)) on the exercise instead of str(number) to create a sequence as this avoids having to cast
+    'd' to an integer when performing the sum, which makes the function faster from ~0.13 to ~0.09, as explained here:
+    https://stackoverflow.com/questions/13905936/converting-integer-to-digit-list
+
+    """
+
+    sequence = list(map(int, str(number)))
+    digits = len(sequence)
+
+    return sum([d**digits for d in sequence]) == number
