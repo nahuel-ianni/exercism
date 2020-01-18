70d9511d7b940102a99ced0787c03edcd5713659
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-18 22:05:00 +0100

    RnaTranscription: Solved exercise

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,9 @@
 def to_rna(dna_strand):
-    pass
+    nucleotide_complements = {
+        "G": "C",
+        "C": "G",
+        "T": "A",
+        "A": "U",
+    }
+
+    return "".join([nucleotide_complements.get(n) for n in dna_strand])
