f19bc66fa0b84cf434d852c385765eccb99d6063
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-22 16:01:12 +0100

    Triangle: Solved exercise

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,16 @@
 def equilateral(sides):
-    pass
-
+    return _guard_triangle_shape(sides) and len(set(sides)) == 1
+    
 
 def isosceles(sides):
-    pass
+    return _guard_triangle_shape(sides) and len(set(sides)) in (1, 2)
 
 
 def scalene(sides):
-    pass
+    return _guard_triangle_shape(sides) and len(set(sides)) == 3
+
+
+def _guard_triangle_shape(sides):
+    s, m, l = sorted(sides)
+
+    return all(side > 0 for side in sides) and s + m >= l 
