90908874e8f4e6025d96212d0def2066ad00ec67
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-22 10:13:08 +0100

    Grains: Solved exercise

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,23 @@
+min_range = 1
+max_range = 64
+
 def square(number):
-    pass
+    if number < min_range or number > max_range:
+        raise ValueError("The requested square is outside the existing range.")
+
+    return _getGrainsAndTotal(number)[0]
 
 
 def total():
-    pass
+    return _getGrainsAndTotal(max_range + 1)[1]
+
+
+def _getGrainsAndTotal(square):
+    grains_sum = 0
+    grains = 1
+    
+    for square in range(min_range, square):
+        grains_sum += grains
+        grains *= 2
+    
+    return (grains, grains_sum)
