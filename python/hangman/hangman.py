f177cecaea4e4ee9b14bd60ed589703defcb49a9
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-22 15:26:40 +0100

    Hangman: Solved exercise

--- python/hangman/hangman.py
+++ python/hangman/hangman.py
@@ -9,12 +9,33 @@ class Hangman:
     def __init__(self, word):
         self.remaining_guesses = 9
         self.status = STATUS_ONGOING
+        self.word = word
+        self.masked_word = "".ljust(len(self.word), "_")
+
 
     def guess(self, char):
-        pass
+        if self.status != STATUS_ONGOING:
+            raise ValueError(f"The game is finished with a {self.status} for the player.")
+
+        if char in self.word and char not in self.masked_word:
+            positions = [p for p, c in enumerate(self.word) if c == char]
+
+            for index in positions:
+                self.masked_word = self.masked_word[:index] + char + self.masked_word[index+1:]
+
+            if self.masked_word == self.word:
+                self.status = STATUS_WIN
+        
+        else:
+            self.remaining_guesses -= 1
+            
+            if self.remaining_guesses < 0:
+                self.status = STATUS_LOSE
+
 
     def get_masked_word(self):
-        pass
+        return self.masked_word
+
 
     def get_status(self):
-        pass
+        return self.status
