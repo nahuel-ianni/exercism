dc1bb6a270163c881b41a1e73c2cb6dd00016482
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-24 22:00:11 +0100

    SaddlePoints: 7/10 tests passing

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,57 @@
 def saddle_points(matrix):
-    pass
+    if len(set([len(row) for row in matrix])) > 1:
+        raise ValueError("Irregular matrices are not supported.")
+
+    inverted_matrix = _invert_matrix(matrix)
+    indexes_row_max = []
+    indexes_col_min = []
+
+    for index, row in enumerate(matrix):
+        indexes_row_max.append([_format_saddle_point(index, i) for i, v in enumerate(row) if v == max(row)])
+
+    for index, column in enumerate(inverted_matrix):
+        indexes_col_min.append([_format_saddle_point(i, index) for i, v in enumerate(column) if v == min(column)])
+
+    print(indexes_row_max)
+    print(indexes_col_min)
+    print("-------------")
+
+    flattened_indexes_row = [item for sublist in indexes_row_max for item in sublist]
+    flattened_indexes_col = [item for sublist in indexes_col_min for item in sublist]
+
+    print(flattened_indexes_row)
+    print(flattened_indexes_col)
+    print("-------------")
+
+    saddle_points = [value for value in flattened_indexes_row if value in flattened_indexes_col] # list(set(flattened_indexes_row) & set(flattened_indexes_col)) #
+
+    print(saddle_points)
+    print("-------------")
+
+    return saddle_points
+
+
+    #=====================================================================================
+    # for index, row in enumerate(matrix):
+    #     for column in inverted_matrix:
+    #         indexes_row_min = [i for i, v in enumerate(row) if v == max(row)]
+    #         indexes_col_max = [i for i, v in enumerate(column) if v == min(column)]#enumerate(max(column))
+
+    #         if indexes_col_max == indexes_row_min:
+    #             saddle_points.append(indexes_row_min)
+    #=====================================================================================
+
+
+def _invert_matrix(matrix):
+    inverted_matrix = []
+    index = 0
+
+    for row in matrix:
+        inverted_matrix.append([row[index] for row in matrix])
+        index += 1
+
+    return inverted_matrix
+
+
+def _format_saddle_point(row, column):
+    return {"row": row + 1, "column": column + 1}
