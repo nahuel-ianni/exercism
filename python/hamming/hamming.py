52bb7e01b353025308b1507361cd7748d81d5c0e
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-24 09:19:55 +0100

    Hamming: Code clean up

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,5 +2,4 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Sequences must be of equal length.")
 
-    zipped_strands = zip(strand_a, strand_b)
-    return sum(n1 != n2 for n1, n2 in zipped_strands)
+    return sum(n1 != n2 for n1, n2 in zip(strand_a, strand_b))
