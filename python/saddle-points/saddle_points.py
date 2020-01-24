47dca2e84f4e81dcc34580e03d4229da91451375
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-24 22:48:57 +0100

    SaddlePoints: Improved solution

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,29 +1,15 @@
 def saddle_points(matrix):
-    if not matrix:
-        return []
-
-    if len(set([len(row) for row in matrix])) > 1:
+    if len(set([len(row) for row in matrix])) > 1: 
         raise ValueError("Irregular matrices are not supported.")
 
-    transposed_matrix = _transpose_matrix(matrix)
-    indexes_row_max = []
-    indexes_col_min = []
+    saddle_points = []
+    row_maxs = [max(x) for x in matrix]
+    col_mins = [min(x) for x in zip(*matrix)]   # zip(*matrix) - Transposes the matrix
+    
+    for r, row_max in enumerate(row_maxs):
+        for c, col_min in enumerate(col_mins):
+            if row_max == col_min:
+                saddle_points.append({"row": r + 1, "column": c + 1})
 
-    for index, row in enumerate(matrix):
-        indexes_row_max.append([_format_saddle_point(index, i) for i, v in enumerate(row) if v == max(row)])
-
-    for index, column in enumerate(transposed_matrix):
-        indexes_col_min.append([_format_saddle_point(i, index) for i, v in enumerate(column) if v == min(column)])
-
-    flattened_indexes_row = [item for sublist in indexes_row_max for item in sublist]
-    flattened_indexes_col = [item for sublist in indexes_col_min for item in sublist]
-
-    return [value for value in flattened_indexes_row if value in flattened_indexes_col]
-
-
-def _transpose_matrix(matrix):
-    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
-
-
-def _format_saddle_point(row, column):
-    return {"row": row + 1, "column": column + 1}
+    return saddle_points
+    
