d15d349c89fef34b67dd06f9b46b24f80beea940
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-25 10:30:18 +0100

    PalindromeProducts: Added guard method

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -1,6 +1,12 @@
 def largest(min_factor, max_factor):
-    pass
+    _guard_min_max(min_factor, max_factor)
 
 
 def smallest(min_factor, max_factor):
-    pass
+    _guard_min_max(min_factor, max_factor)
+
+
+
+def _guard_min_max(min_factor, max_factor):
+    if min_factor > max_factor:
+        raise ValueError("The order of the parameter is inverted.")
