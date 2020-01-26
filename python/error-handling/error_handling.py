cf6c3ecd66d042a972236f7a27e66ac7f9b72c12
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-26 11:12:12 +0100

    ErrorHandling: Solved exercise

--- python/error-handling/error_handling.py
+++ python/error-handling/error_handling.py
@@ -1,14 +1,32 @@
 def handle_error_by_throwing_exception():
-    pass
+    raise Exception("Really useful description.")
 
 
 def handle_error_by_returning_none(input_data):
-    pass
+    value = None
+
+    try:
+        value = int(input_data)
+    
+    finally:    
+        return value
 
 
 def handle_error_by_returning_tuple(input_data):
-    pass
+    operation_successful = False
+    value = input_data
+
+    try:
+        value = int(input_data)
+        operation_successful = True
+    
+    finally:
+        return (operation_successful, value)
 
 
 def filelike_objects_are_closed_on_exception(filelike_object):
-    pass
+    try:
+        filelike_object.do_something()
+
+    finally:
+        filelike_object.close()
