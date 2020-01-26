a3627ab7c6499b752fcbe80238c773bec6db312e
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-26 16:05:37 +0100

    Bob: Beautified exercise

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -2,27 +2,16 @@ def response(hey_bob):
     phrase = hey_bob.strip()
     response = "Whatever."
 
-    if not phrase:
+    if phrase.isupper() and phrase.endswith("?"):
+        response = "Calm down, I know what I'm doing!"
+
+    elif phrase.isupper():
+        response = "Whoa, chill out!"
+    
+    elif phrase.endswith("?"):
+        response = "Sure."
+    
+    elif not phrase:
         response = "Fine. Be that way!"
 
-    elif phrase[-1] == "?":
-        if _char_are_uppercase(phrase) and _sentence_has_chars(phrase):
-            response =  "Calm down, I know what I'm doing!"
-        
-        else:
-            response = "Sure."
-
-    elif _char_are_uppercase(phrase) and _sentence_has_chars(phrase):
-            response = "Whoa, chill out!"
-
     return response
-
-
-def _sentence_has_chars(phrase):
-    return any([c.isalpha() for c in phrase])
-
-
-def _char_are_uppercase(phrase):
-    phrase = "".join([c for c in phrase if c.isalpha()])
-    
-    return phrase == phrase.upper() 
