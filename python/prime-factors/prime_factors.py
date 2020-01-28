5a6902ef137100f469922f642899b880fbd1022e
Nahuel Ianni <service.nahuel@outlook.com.misc>
2020-01-28 18:37:00 +0100

    PrimeFactors: Changed if-else to while function

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -2,12 +2,11 @@ def factors(value):
     prime_factors = []
     divisor = 2
 
-    while value != 1 or divisor <= value:
-        if value % divisor == 0:
+    while divisor <= value:
+        while value % divisor == 0:
             value /= divisor
             prime_factors.append(divisor)
     
-        else:
-            divisor += 1
+        divisor += 1
 
     return prime_factors
