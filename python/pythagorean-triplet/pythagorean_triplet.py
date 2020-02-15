159816132355b1326d189354ee2f2e8342a21efb
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-02-16 00:00:26 +0100

    PythagoreanTriplet: Solved exercise

--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,10 +1,20 @@
 def triplets_with_sum(number):
-    pass
+    return [t for t in triplets_in_range(1, number) if sum(t) == number]
+
 
 
 def triplets_in_range(start, end):
-    pass
+    triplets = []
+
+    for n in range(start, end - 3):
+        for m in range (n + 1, end - 2):
+            triplet = [n, m, end - n - m]
+
+            if is_triplet(triplet):
+                triplets.append(triplet)
+
+    return triplets
 
 
 def is_triplet(triplet):
-    pass
+    return triplet[0] ** 2 + triplet[1] ** 2 == triplet[2] ** 2 and triplet[0] < triplet[1] < triplet[2]
