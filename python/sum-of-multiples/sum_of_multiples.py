c8af4e8e09d82907bdeb8fd280a5a8fb1ccf5c0a
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-02-15 17:22:00 +0100

    SumOfMultiples: Solved exercise

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,4 @@
 def sum_of_multiples(limit, multiples):
-    pass
+    multiples = [i for i in multiples if i != 0]
+
+    return sum(i for i in range(0, limit) if next((m for m in multiples if i % m == 0), 0) != 0)
