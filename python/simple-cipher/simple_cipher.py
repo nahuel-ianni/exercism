71cd175ca916aa1974608fe3f81125ff2d0fd5ef
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-02-15 14:27:41 +0100

    SimpleCipher: Solved exercise

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -1,9 +1,27 @@
+from secrets import choice
+from string import ascii_lowercase as alphabet
+
+
 class Cipher:
     def __init__(self, key=None):
-        pass
+        self.key = key or "".join(choice(alphabet) for _ in range(100))
+
 
     def encode(self, text):
-        pass
+        value = ""
+
+        for i in range(len(text)):
+            key = self.key[i % len(self.key)]
+            value += alphabet[(alphabet.index(text[i]) + alphabet.index(key)) % len(alphabet)]
+
+        return value
+
 
     def decode(self, text):
-        pass
+        value = ""
+
+        for i in range(len(text)):
+            key = self.key[i % len(self.key)]
+            value += alphabet[(alphabet.index(text[i]) - alphabet.index(key)) % len(alphabet)]
+        
+        return value
