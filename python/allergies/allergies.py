814c6c3b381843467bb010145f131f2044dc57d3
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-02-16 11:19:56 +0100

    Allergies: Solved exercise

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,11 +1,21 @@
 class Allergies:
+    allergies = {
+        "eggs"         : 1,
+        "peanuts"      : 2,
+        "shellfish"    : 4,
+        "strawberries" : 8,
+        "tomatoes"     : 16,
+        "chocolate"    : 32,
+        "pollen"       : 64,
+        "cats"         : 128,
+    }
 
     def __init__(self, score):
-        pass
+        self.score = score
 
     def allergic_to(self, item):
-        pass
+        return self.score & self.allergies[item] != 0
 
     @property
     def lst(self):
-        pass
+        return [allergy for allergy in self.allergies.keys() if self.allergic_to(allergy)]
