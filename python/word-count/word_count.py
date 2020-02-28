6bbce734cdb86d595657aac886aaf1349ef336fd
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-02-28 21:21:15 +0100

    WordCount: Solved exercise

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,13 @@
+from re import sub
+from string import punctuation
+
+
 def count_words(sentence):
-    pass
+    word_count = {}
+    sentence = sub(f"[{punctuation}]".replace("'", ""), " ", sentence.lower())
+
+    for word in sentence.split():
+        word = word.strip("'")
+        word_count[word] = sum(1 for w in sentence.split() if w.strip("'") == word)
+
+    return word_count
