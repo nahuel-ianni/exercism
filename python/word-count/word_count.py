9d31c56e20f852e8eb564e06e5a3289e86a08787
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-02-28 21:31:31 +0100

    WordCount: Made code prettier

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -5,9 +5,9 @@ from string import punctuation
 def count_words(sentence):
     word_count = {}
     sentence = sub(f"[{punctuation}]".replace("'", ""), " ", sentence.lower())
+    sentence = [word.strip("'") for word in sentence.split()]
 
-    for word in sentence.split():
-        word = word.strip("'")
-        word_count[word] = sum(1 for w in sentence.split() if w.strip("'") == word)
+    for word in sentence:
+        word_count[word] = sum(1 for w in sentence if w == word)
 
     return word_count
