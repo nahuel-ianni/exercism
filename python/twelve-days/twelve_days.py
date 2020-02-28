f4188f59b90a737760cd0acdf7f1619a56d3914c
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-02-28 10:39:04 +0100

    TwelveDays: Updated according to feedback

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,17 +1,19 @@
-presents = {
-    1  : ("first"   , "a Partridge in a Pear Tree"),
-    2  : ("second"  , "two Turtle Doves"),
-    3  : ("third"   , "three French Hens"),
-    4  : ("fourth"  , "four Calling Birds"),
-    5  : ("fifth"   , "five Gold Rings"),
-    6  : ("sixth"   , "six Geese-a-Laying"),
-    7  : ("seventh" , "seven Swans-a-Swimming"),
-    8  : ("eighth"  , "eight Maids-a-Milking"),
-    9  : ("ninth"   , "nine Ladies Dancing"),
-    10 : ("tenth"   , "ten Lords-a-Leaping"),
-    11 : ("eleventh", "eleven Pipers Piping"),
-    12 : ("twelfth" , "twelve Drummers Drumming"),
-}
+days = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
+
+presents = [    
+    "a Partridge in a Pear Tree",
+    "two Turtle Doves",
+    "three French Hens",
+    "four Calling Birds",
+    "five Gold Rings",
+    "six Geese-a-Laying",
+    "seven Swans-a-Swimming",
+    "eight Maids-a-Milking",
+    "nine Ladies Dancing",
+    "ten Lords-a-Leaping",
+    "eleven Pipers Piping",
+    "twelve Drummers Drumming",
+]
 
 
 def recite(start_verse, end_verse):
@@ -19,8 +21,7 @@ def recite(start_verse, end_verse):
 
 
 def compose_lyrics(verse):
-    lyrics = f"On the {presents[verse][0]} day of Christmas my true love gave to me: "
-    daily_phrase = [f"{phrase[1]}," for index, phrase in enumerate(presents.values()) if index in range(0, verse)]
-    daily_phrase[0] = f"and {daily_phrase[0][:-1]}." if len(daily_phrase) > 1 else f"{daily_phrase[0][:-1]}."
+    lyrics = [phrase for index, phrase in enumerate(presents) if index in range(0, verse)]
+    if verse > 1: lyrics[0] = f"and {lyrics[0]}"
     
-    return lyrics + " ".join(reversed(daily_phrase))
+    return f'On the {days[verse - 1]} day of Christmas my true love gave to me: {", ".join(reversed(lyrics))}.'
