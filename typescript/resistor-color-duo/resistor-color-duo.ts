9623fcd412dc55e0a0a9b5693663b95b3fc01bef
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-01 18:53:59 +0100

    ResistorColor: Solved exercise

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,8 +1,33 @@
 export class ResistorColor {
   private colors: string[];
+  private table = new Map<string, number>(
+    [
+      [ "black" , 0 ],
+      [ "brown" , 1 ],
+      [ "red"   , 2 ],
+      [ "orange", 3 ],
+      [ "yellow", 4 ],
+      [ "green" , 5 ],
+      [ "blue"  , 6 ],
+      [ "violet", 7 ],
+      [ "grey"  , 8 ],
+      [ "white" , 9 ],
+    ]);
 
   constructor(colors: string[]) {
+    if (colors.length < 2)
+      throw new Error("At least two colors need to be present");
+
     this.colors = colors;
   }
-  value = (): number => 0;
+
+  value = (): number => {
+    const value1 = this.table.get(this.colors[0]);
+    const value2 = this.table.get(this.colors[1]);
+
+    if (value1 != null && value2 != null)
+      return 10 * value1 + value2;
+    
+    throw new Error("At least two colors need to be present");
+  }
 }
