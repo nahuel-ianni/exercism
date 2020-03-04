f1b750d7f733f7a5f63c96dac1e3d515be09be8e
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-04 14:48:05 +0100

    Sieve: Solved exercise

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,2 +1,14 @@
-def primes(limit):
-    pass
+def primes(limit, p = 2):
+    numbers = [n for n in range(p, limit + 1)]
+
+    while p < limit + 1:
+        for value in range(p, limit + 1, p):
+            value = value + p
+            if value not in numbers: 
+                continue
+            
+            numbers[numbers.index(value)] = 0
+
+        p += 1
+
+    return [n for n in numbers if n != 0]
