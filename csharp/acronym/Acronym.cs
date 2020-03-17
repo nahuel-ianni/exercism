a9ea09fcb2af43dd2b5fe5d757a22f8972005093
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-17 09:37:11 +0100

    Acronym: Added and solved exercise

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+public static class Acronym
+{
+    static char[] delimiterChars = { ' ', '-', '_' };
+
+    public static string Abbreviate(string phrase) =>
+        string.Concat(
+            from word in phrase.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries)
+            select word.Substring(0, 1).ToUpper());
+}
