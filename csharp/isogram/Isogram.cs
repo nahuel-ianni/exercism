2e20a07538dd45c01fc43f43c6bb56f9ee4e3e63
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-17 08:21:49 +0100

    Isogram: Improved solution

--- csharp/isogram/Isogram.cs
+++ csharp/isogram/Isogram.cs
@@ -4,8 +4,9 @@ public static class Isogram
 {
     public static bool IsIsogram(string word) 
     {
-        word = word.ToLower().Replace("-", string.Empty).Replace(" ", string.Empty);
-
-        return word.Length == word.Distinct().Count();    
+        return word.Where(c => char.IsLetter(c))
+                   .Select(c => char.ToLower(c))
+                   .GroupBy(c => c)
+                   .All(group => group.Count() == 1);
     }
 }
