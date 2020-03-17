44afd62d09fbfa971b742ea407d50e32b147ed40
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-17 08:11:42 +0100

    Isogram: Added and solved exercise

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word) 
+    {
+        word = word.ToLower().Replace("-", string.Empty).Replace(" ", string.Empty);
+
+        return word.Length == word.Distinct().Count();    
+    }
+}
