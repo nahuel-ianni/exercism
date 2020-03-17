d9d357b0737afefa09db8f1176fe13027dfe95f5
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-17 08:34:21 +0100

    Pangram: Added and solved exercise

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+public static class Pangram
+{
+    const int AlphabetLength = 26;
+
+    public static bool IsPangram(string input) =>
+        input.Where(c => char.IsLetter(c))
+             .Select(c => char.ToLower(c))
+             .GroupBy(c => c)
+             .Count() == Pangram.AlphabetLength;
+}
