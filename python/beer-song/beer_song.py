8f702233068aa1409434c75c4f17a450a7e79a18
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-18 18:24:32 +0100

    BeerSong: Added and solved exercise

---
+++ python/beer-song/beer_song.py
@@ -0,0 +1,33 @@
+def recite(start, take=1):
+    lyrics = []
+
+    while take > 0:
+        if start == 0:
+            lyrics += [
+                "No more bottles of beer on the wall, no more bottles of beer.",
+                f"Go to the store and buy some more, 99 bottles of beer on the wall."]
+            
+            break
+        
+        lyrics += [
+            f"{bottle_num(start)} {bottle_txt(start)} of beer on the wall, {bottle_num(start)} {bottle_txt(start)} of beer.",
+            f"Take {bottle_qty(start - 1)} down and pass it around, {bottle_num(start - 1)} {bottle_txt(start - 1)} of beer on the wall."]
+        
+        take -= 1
+        start -= 1
+
+        if take > 0: lyrics.append("")
+
+    return lyrics
+
+
+def bottle_num(number):
+    return f"{number}" if number > 0 else "no more"
+
+
+def bottle_txt(number):
+    return "bottles" if number != 1 else "bottle"
+
+
+def bottle_qty(number):
+    return "one" if number > 0 else "it"
