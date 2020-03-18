bd8516da3b6f83ed782a7f972b7f9dc284780095
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-18 06:28:31 +0100

    Leap: Improved solution

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,6 +1,6 @@
 def leap_year(year):
     check1 = is_divisible(year, 4) and not is_divisible(year, 100)
-    check2 = is_divisible(year, 100) and is_divisible(year, 400)
+    check2 = is_divisible(year, 400)
 
     return check1 or check2
 
