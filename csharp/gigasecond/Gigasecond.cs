2b5c235a7db4d9597a3a50260736923133e2871f
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-18 13:06:38 +0100

    Gigasecond: Readability

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -3,5 +3,6 @@ using System;
 public static class Gigasecond
 {
     // Math.Pow(10, 9) == 1e9
-    public static DateTime Add(DateTime moment) => moment.AddSeconds(1e9);
+    public static DateTime Add(DateTime moment) => 
+        moment.AddSeconds(1e9);
 }
