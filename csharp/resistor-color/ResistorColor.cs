ea445f1ef25c6b02be35f7bff5a78ae87a770fa0
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-18 16:31:09 +0100

    ResistorColor: Improved solution

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -2,13 +2,14 @@
 
 public static class ResistorColor
 {
-    public static int ColorCode(string color) =>
-        Array.IndexOf(Colors(), color);
-
-    public static string[] Colors() =>
-        new string[]
+    static readonly string[] colors = new string[]
         { 
             "black", "brown", "red", "orange", "yellow",
             "green", "blue", "violet", "grey", "white"
         };
+
+    public static int ColorCode(string color) =>
+        Array.IndexOf(colors, color);
+
+    public static string[] Colors() => colors;
 }
