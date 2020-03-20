ff3f48bf802d4ca1e5ec8cf64f47af094d6ce2bc
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-20 15:58:37 +0100

    SpaceAge: Removed white space

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -14,7 +14,7 @@ public class SpaceAge
         spaceAgeOnMercury = CalculateYears(seconds, 0.2408467  * yearlySecondsOnEarth);
         spaceAgeOnVenus   = CalculateYears(seconds, 0.61519726 * yearlySecondsOnEarth);
         spaceAgeOnMars    = CalculateYears(seconds, 1.8808158  * yearlySecondsOnEarth);
-        spaceAgeOnJupiter = CalculateYears(seconds,  11.862615 * yearlySecondsOnEarth);
+        spaceAgeOnJupiter = CalculateYears(seconds, 11.862615  * yearlySecondsOnEarth);
         spaceAgeOnSaturn  = CalculateYears(seconds, 29.447498  * yearlySecondsOnEarth);
         spaceAgeOnUranus  = CalculateYears(seconds, 84.016846  * yearlySecondsOnEarth);
         spaceAgeOnNeptune = CalculateYears(seconds, 164.79132  * yearlySecondsOnEarth);
@@ -28,7 +28,7 @@ public class SpaceAge
     public double OnSaturn() => spaceAgeOnSaturn;
     public double OnUranus() => spaceAgeOnUranus;
     public double OnNeptune() => spaceAgeOnNeptune;
-    
+
     private static double CalculateYears(int seconds, double planetarySeconds)
     {
         return Math.Round(seconds / planetarySeconds, 2);
