acf53395840d67164140e00f0439a165623c86c3
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-20 15:10:43 +0100

    SpaceAge: Improved solution

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -2,44 +2,38 @@ using System;
 
 public class SpaceAge
 {
-    const double orbitalPeriodEarth   = 1;
-    const double orbitalPeriodMercury = 0.2408467;
-    const double orbitalPeriodVenus   = 0.61519726;
-    const double orbitalPeriodMars    = 1.8808158;
-    const double orbitalPeriodJupiter = 11.862615;
-    const double orbitalPeriodSaturn  = 29.447498;
-    const double orbitalPeriodUranus  = 84.016846;
-    const double orbitalPeriodNeptune = 164.79132;
     const int yearlySecondsOnEarth = 31557600;
+    const double planetarySecondsMercury = 0.2408467 * yearlySecondsOnEarth;
+    const double planetarySecondsVenus   = 0.61519726 * yearlySecondsOnEarth;
+    const double planetarySecondsMars    = 1.8808158 * yearlySecondsOnEarth;
+    const double planetarySecondsJupiter = 11.862615 * yearlySecondsOnEarth;
+    const double planetarySecondsSaturn  = 29.447498 * yearlySecondsOnEarth;
+    const double planetarySecondsUranus  = 84.016846 * yearlySecondsOnEarth;
+    const double planetarySecondsNeptune = 164.79132 * yearlySecondsOnEarth;
 
     readonly int seconds;
 
     public SpaceAge(int seconds) => 
         this.seconds = seconds;
 
-    public double OnEarth() => this.CalculateYears(orbitalPeriodEarth);
+    public double OnEarth() => this.CalculateYears(yearlySecondsOnEarth);
 
-    public double OnMercury() => this.CalculateYears(orbitalPeriodMercury);
+    public double OnMercury() => this.CalculateYears(planetarySecondsMercury);
 
-    public double OnVenus() => this.CalculateYears(orbitalPeriodVenus);
+    public double OnVenus() => this.CalculateYears(planetarySecondsVenus);
 
-    public double OnMars() => this.CalculateYears(orbitalPeriodMars);
+    public double OnMars() => this.CalculateYears(planetarySecondsMars);
 
-    public double OnJupiter() => this.CalculateYears(orbitalPeriodJupiter);
+    public double OnJupiter() => this.CalculateYears(planetarySecondsJupiter);
 
-    public double OnSaturn() => this.CalculateYears(orbitalPeriodSaturn);
+    public double OnSaturn() => this.CalculateYears(planetarySecondsSaturn);
 
-    public double OnUranus() => this.CalculateYears(orbitalPeriodUranus);
+    public double OnUranus() => this.CalculateYears(planetarySecondsUranus);
 
-    public double OnNeptune() => this.CalculateYears(orbitalPeriodNeptune);
+    public double OnNeptune() => this.CalculateYears(planetarySecondsNeptune);
 
-    private double CalculateOrbitalPeriodToSeconds(double orbitalPeriod) =>
-        orbitalPeriod * yearlySecondsOnEarth;
-
-    private double CalculateYears(double orbitalPeriod)
+    private double CalculateYears(double planetarySeconds)
     {
-        var planetarySeconds = this.CalculateOrbitalPeriodToSeconds(orbitalPeriod);
-
         return Math.Round(this.seconds / planetarySeconds, 2);
     }
 }
