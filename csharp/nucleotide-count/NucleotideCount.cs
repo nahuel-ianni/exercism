cb265612dea63e59897d284ebd8638d6b3ff7aec
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-22 10:19:03 +0100

    NucletoideCount: Solved exercise with Linq

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -1,10 +1,18 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public static class NucleotideCount
 {
+    const string ValidNucletoides = "ACGT";
+
     public static IDictionary<char, int> Count(string sequence)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (sequence.Except(ValidNucletoides).Count() > 0)
+            throw new ArgumentException("Sequence contains invalid characters.");
+        
+        return ValidNucletoides
+                .Select(x => new KeyValuePair<char, int>(x, sequence.Count(y => y == x)))
+                .ToDictionary(t => t.Key, t => t.Value);
     }
 }
