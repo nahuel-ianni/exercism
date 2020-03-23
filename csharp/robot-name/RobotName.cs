51d1b44a142cab1de5f183cb2b6a28c1ae24e94e
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-23 09:06:25 +0100

    RobotName: Solved exercise

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,17 +1,41 @@
 using System;
+using System.Collections.Generic;
 
 public class Robot
 {
+    const string AllowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const int MaxDigit = 1000;
+
+    public static IList<string> Names = new List<string>();
+
+    Random random = new Random();
+
     public string Name
     {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+        get;
+        internal set;
     }
 
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string name;
+
+        do
+        {
+            var chars = $"{GetRandomChar()}{GetRandomChar()}";
+            var digit = random.Next(MaxDigit).ToString("D3");
+
+            name = $"{chars}{digit}";
+        }
+        while(Names.Contains(name));
+
+        Names.Remove(Name);
+        Names.Add(name);
+        Name = name;
     }
+
+    public Robot() => Reset();
+
+    char GetRandomChar() =>
+        AllowedChars[random.Next(AllowedChars.Length)];
 }
