cf47443515d14517449aacd73286aaec2d3a8de7
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-24 13:50:34 +0100

    Allergies: Solved exercise

--- csharp/allergies/Allergies.cs
+++ csharp/allergies/Allergies.cs
@@ -1,30 +1,30 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public enum Allergen
 {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats
+    Eggs         = 1,
+    Peanuts      = 2,
+    Shellfish    = 4,
+    Strawberries = 8,
+    Tomatoes     = 16,
+    Chocolate    = 32,
+    Pollen       = 64,
+    Cats         = 128
 }
 
 public class Allergies
 {
-    public Allergies(int mask)
-    {
-    }
+    readonly IEnumerable<Allergen> allergens = Enum.GetValues(typeof(Allergen)).Cast<Allergen>().AsEnumerable();
+    readonly int mask;
 
-    public bool IsAllergicTo(Allergen allergen)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public Allergies(int mask) =>
+        this.mask = mask;
 
-    public Allergen[] List()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public bool IsAllergicTo(Allergen allergen) =>
+        (this.mask & (int)allergen) != 0;
+
+    public Allergen[] List() =>
+        allergens.Where(x => IsAllergicTo(x)).ToArray();
 }
