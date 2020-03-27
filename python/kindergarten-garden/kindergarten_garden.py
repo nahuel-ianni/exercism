abfd3d9170a19e19c8cb848657808627f91f98d6
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-27 10:59:42 +0100

    KindergartenGarden: Solved exercise

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,37 @@
+from enum import Enum, unique
+
+
+@unique
+class Plants(Enum):
+    C = "Clover"
+    G = "Grass"
+    R = "Radishes"
+    V = "Violets"
+
+
 class Garden:
-    def __init__(self, diagram, students):
-        pass
+    def __init__(self, diagram, students=None):
+        self.diagram = diagram.split("\n")
+        self.students = sorted(students) if students else [
+            "Alice", "Bob", "Charlie", "David", "Eve", "Fred",
+            "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"
+        ]
+
+    def plants(self, student):
+        if student not in self.students:
+            raise ValueError(f"Student {student} couldn't be found.")
+
+        # Nested for loops
+        plants = []
+        index = self.students.index(student) * 2
+
+        for keys in self.diagram:
+            for cup in range(index, index + 2):
+                key = keys[cup]
+                plants.append(Plants[key].value)
+
+        return plants
+
+        # Nested list comprehension
+        # index = self.students.index(student) * 2
+        # return [Plants[keys[cup]].value for keys in self.diagram for cup in range(index, index + 2)]
