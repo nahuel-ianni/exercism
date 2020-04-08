ca4eb0ac37955579d2365a747f23bd7e19c0b652
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-04-08 11:38:25 +0200

    TwoFer: Removed comments

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,10 +1,3 @@
-/*
-A string format in JavaScript, as used in this exercise
-is called a "Template literal".
-
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-*/
-
 export const twoFer = (name = "you") => {
   return `One for ${name}, one for me.`
 };
