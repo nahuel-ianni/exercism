3a6c12525b43c7ad5e26f4437a6e4db6df1115ff
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-04-08 14:10:24 +0200

    Clock: Several tests passing

--- csharp/clock/Clock.cs
+++ csharp/clock/Clock.cs
@@ -2,18 +2,46 @@ using System;
 
 public class Clock
 {
+    private int hours, minutes = 0;
+
     public Clock(int hours, int minutes)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        _ = hours >= 0
+            ? Add(hours * 60)
+            : Subtract(hours * 60);
+
+        _ = minutes >= 0
+            ? Add(minutes)
+            : Subtract(minutes);
+
+        // this.minutes = minutes >= 0
+        //     ? minutes % 60
+        //     : 60 - ((minutes * -1) % 60);
+
+        // this.hours = hours >= 0
+        //     ? (hours + minutes / 60) % 24
+        //     : 24 - ((hours * -1) % 24);
     }
 
-    public Clock Add(int minutesToAdd)
+    public Clock Add(int minutes)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        this.minutes += minutes % 60;
+        this.hours += (minutes / 60) % 24;
+
+        return this;
     }
 
-    public Clock Subtract(int minutesToSubtract)
+    public Clock Subtract(int minutes)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        this.minutes = (60 - (minutes * -1)) % 60;
+        this.hours = (24 - (((minutes * -1) / 60) % 24)) % 24;
+
+        return this;
     }
+
+    public override bool Equals(object obj) =>
+        this.ToString() == obj.ToString();
+
+    public override string ToString() =>
+        $"{hours.ToString("D2")}:{minutes.ToString("D2")}";
 }
