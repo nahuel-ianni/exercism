c13548c3a71022fb9caf62ec6d4d028300382b8d
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-04-08 11:31:07 +0200

    ResistorColorDuo: Improved solution

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,6 +1,5 @@
-export const decodedValue = (colors) => {
-  return COLORS.indexOf(colors[0]) * 10 + COLORS.indexOf(colors[1])
-};
+export const decodedValue = ([c1, c2]) =>
+  COLORS.indexOf(c1) * 10 + COLORS.indexOf(c2);
 
 export const COLORS = [
   "black","brown","red","orange","yellow",
