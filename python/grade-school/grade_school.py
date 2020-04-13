054d6ca7719bfd8934acaa43e159f4b67b1d3172
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-04-13 21:23:49 +0200

    GradeSchool: Solved exercise

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,12 +1,19 @@
 class School:
     def __init__(self):
-        pass
+        self.students = {}
 
     def add_student(self, name, grade):
-        pass
+        if self._grade_exists(grade):
+            self.students[grade].append(name)
+            self.students[grade].sort()
+        else:
+            self.students[grade] = [name]
 
     def roster(self):
-        pass
+        return [name for k, values in sorted(self.students.items()) for name in values]
 
-    def grade(self, grade_number):
-        pass
+    def grade(self, grade):
+        return self.students[grade] if self._grade_exists(grade) else []
+
+    def _grade_exists(self, grade):
+        return grade in self.students.keys()
