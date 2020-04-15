4577f7df1e3b3029da0781841bcf8507146d1faa
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-04-15 10:10:50 +0200

    SpaceAge: Solved exercise

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,8 +1,15 @@
-//
-// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const earthSeconds = 31557600;
 
-export const age = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+const PlanetarySeconds = {
+  earth   : earthSeconds,
+  mercury : 0.2408467  * earthSeconds,
+  venus   : 0.61519726 * earthSeconds,
+  mars    : 1.8808158  * earthSeconds,
+  jupiter : 11.862615  * earthSeconds,
+  saturn  : 29.447498  * earthSeconds,
+  uranus  : 84.016846  * earthSeconds,
+  neptune : 164.79132  * earthSeconds
+}
+
+export const age = (planet, seconds) => 
+  Number((seconds / PlanetarySeconds[planet]).toFixed(2));
