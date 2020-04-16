a557bbff032e7641c001c138ba668b59a8645f90
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-04-16 19:40:39 +0200

    Triangle: Solved exercise

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,22 +1,22 @@
-//
-// This is only a SKELETON file for the 'Triangle' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Triangle {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(a, b, c) {
+    this.sides = [a, b, c].sort();
   }
 
   isEquilateral() {
-    throw new Error("Remove this statement and implement this function");
+    return this._guard_shape() && [...new Set(this.sides)].length == 1;
   }
 
   isIsosceles() {
-    throw new Error("Remove this statement and implement this function");
+    return this._guard_shape() && [1, 2].includes([...new Set(this.sides)].length);
   }
 
   isScalene() {
-    throw new Error("Remove this statement and implement this function");
+    return this._guard_shape() && [...new Set(this.sides)].length == 3;
+  }
+
+  _guard_shape(){
+    return this.sides.every((x) => x > 0) &&
+           this.sides[0] + this.sides[1] >= this.sides[2];
   }
 }
