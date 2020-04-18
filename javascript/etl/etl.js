d1f9bae347432b51b829942cccabc9823440ecf8
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-04-18 20:40:21 +0200

    ETL: Cleaned up symbols

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,10 +1,9 @@
 export const transform = (oldTable) => {
   const scoreTable = {};
 
-  for (const score in oldTable){
-    oldTable[score].forEach((ch) =>
-      scoreTable[ch.toLowerCase()] = Number(score)
-    );
+  for (const score in oldTable) {
+    oldTable[score].forEach(ch =>
+      scoreTable[ch.toLowerCase()] = Number(score));
   }
 
   return scoreTable;
