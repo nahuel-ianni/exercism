4548456c8dde9ab3e4962f78465797f6c9893b85
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-04-18 20:56:31 +0200

    Raindrops: Simplified code

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -1,15 +1,8 @@
 export const convert = (raindrop) => {
-  let sound = raindrop % 3 == 0 
-    ? "Pling"
-    : "";
+  let sound = raindrop % 3 == 0 ? "Pling" : "";
+  
+  if (raindrop % 5 == 0) sound += "Plang";
+  if (raindrop % 7 == 0) sound += "Plong";
 
-  if (raindrop % 5 == 0)
-    sound += "Plang";
-
-  if (raindrop % 7 == 0)
-    sound += "Plong";
-
-  return sound 
-    ? sound
-    : raindrop.toString();
+  return sound || raindrop.toString();
 };
