5a5e9750305b05ea328a95e2b89ca1a17dde0a66
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-04-24 14:17:03 +0200

    Say: Solved numbers 0-13

--- python/say/say.py
+++ python/say/say.py
@@ -1,2 +1,29 @@
+from enum import Enum
+
+
+class Numbers(Enum):
+    zero     = 0
+    one      = 1
+    two      = 2
+    three    = 3
+    four     = 4
+    five     = 5
+    six      = 6
+    seven    = 7
+    eight    = 8
+    nine     = 9
+    ten      = 10
+    eleven   = 11
+    twelve   = 12
+    thirteen = 13
+
 def say(number):
-    pass
+    output = ""
+
+    if number < 0 or number > 999999999999:
+        raise ValueError("Value out of range")
+
+    if number < 14:
+        output = Numbers(number).name
+    
+    return output
