f677eea8fc1b1802b6a60ad1320fa0b9cbe4ae77
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-04-29 06:03:37 +0200

    GradeSchool: Changed solution to use defaultdict

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,19 +1,19 @@
+# Default dict removes the issue of inserting keys for the first time vs appending items
+# as well as returning an empty list (by initializing it with a list argument)
+# for a key that doesn't exist
+from collections import defaultdict
+
+
 class School:
     def __init__(self):
-        self.students = {}
+        self.students = defaultdict(list)
 
     def add_student(self, name, grade):
-        if self._grade_exists(grade):
-            self.students[grade].append(name)
-            self.students[grade].sort()
-        else:
-            self.students[grade] = [name]
+        self.students[grade].append(name)
+        self.students[grade].sort()
 
     def roster(self):
         return [name for k, values in sorted(self.students.items()) for name in values]
 
     def grade(self, grade):
-        return self.students[grade] if self._grade_exists(grade) else []
-
-    def _grade_exists(self, grade):
-        return grade in self.students.keys()
+        return self.students[grade]
