7f85976fe393df8b8da45098389a13a0c3ff7d85
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-04-29 19:37:22 +0200

    Pangram: Solved exercise

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,8 +1,5 @@
-//
-// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const isPangram = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+export const isPangram = (phrase) =>
+  new Set(
+    phrase
+      .toLowerCase()
+      .match(/[a-z]/gi)).size == 26;
