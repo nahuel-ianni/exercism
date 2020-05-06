feb4db3ee59831e64ab7a0da9a9cfd3697621399
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-05-06 16:04:18 +0200

    Bob: Solved exercise

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,9 +1,30 @@
-using System;
+using System.Linq;
 
 public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        statement = statement.Trim();
+        var isUpper = IsUpper(statement);
+        var output = "Whatever.";
+
+        if (string.IsNullOrEmpty(statement))
+            output = "Fine. Be that way!";
+
+        else if (statement.EndsWith("?"))
+            output = isUpper
+                ? "Calm down, I know what I'm doing!"
+                : "Sure.";
+
+        else if (isUpper)
+            output = "Whoa, chill out!";
+
+        return output;
+    }
+
+    private static bool IsUpper(string s)
+    {
+        return s.Any(c => char.IsLetter(c)) &&
+               s.Where(c => char.IsLetter(c)).All(c => char.IsUpper(c));
     }
 }
