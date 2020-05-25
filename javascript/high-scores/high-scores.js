3d94471b4bee2b426f52469e9dbf1f280494e4bd
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-05-25 20:43:16 +0200

    HighScores: Missing top three method

--- javascript/high-scores/high-scores.js
+++ javascript/high-scores/high-scores.js
@@ -1,26 +1,21 @@
-//
-// This is only a SKELETON file for the 'High Scores' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class HighScores {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(input) {
+    this._scores = input;
   }
 
   get scores() {
-    throw new Error("Remove this statement and implement this function");
+    return this._scores;
   }
 
   get latest() {
-    throw new Error("Remove this statement and implement this function");
+    return this._scores[this._scores.length - 1];
   }
 
   get personalBest() {
-    throw new Error("Remove this statement and implement this function");
+    return Math.max(...this._scores);
   }
 
   get personalTopThree() {
-    throw new Error("Remove this statement and implement this function");
+    
   }
 }
