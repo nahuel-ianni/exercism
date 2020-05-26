98f61140005a94b1ba373b18a169c236978a2a51
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-05-26 19:29:33 +0200

    Matchingbrackets: Solved exercise

--- csharp/matching-brackets/MatchingBrackets.cs
+++ csharp/matching-brackets/MatchingBrackets.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class MatchingBrackets
 {
     public static bool IsPaired(string input)
     {
-        throw new NotImplementedException();
+        var curatedInput = input
+            .Where(c => c.Equals('(') || c.Equals(')') ||
+                        c.Equals('[') || c.Equals(']') ||
+                        c.Equals('{') || c.Equals('}'))
+            .ToArray();
+
+        var brackets = string.Concat(curatedInput);
+
+        while (brackets.Contains("()") || brackets.Contains("[]") || brackets.Contains("{}"))
+            brackets = brackets.Replace("()", "").Replace("[]", "").Replace("{}", "");
+
+        return brackets.Length == 0;
     }
 }
