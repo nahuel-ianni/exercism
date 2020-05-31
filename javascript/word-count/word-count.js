27d89725152289f9365060da678355bd1ace2306
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-05-31 13:49:11 +0200

    WordCount: Solved exercise

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,12 +1,11 @@
 export const countWords = (phrase) => {
   const words = Object.create(null);
-  phrase = phrase.match(/\w+/g);
+  phrase = phrase.toLowerCase().match(/([\w]+['][\w]+)|\w+/g);
 
   phrase.forEach(word => {
     if (words[word]) words[word] += 1;
     else words[word] = 1;
   });
 
-  console.log(words);
   return words;
 };
