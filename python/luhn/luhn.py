00b4cdd91c83ea5ee1411446f9c233691e6dc60d
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-06-05 14:45:54 +0200

    Luhn: Simplified code

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -3,7 +3,7 @@ class Luhn:
         self.card = card_num.replace(" ", "")
 
     def valid(self):
-        if not self._guardInput():
+        if not (self.card.isdigit() and len(self.card) > 1):
             return False
 
         digits = list(reversed(self._curateInput(self.card)))
@@ -12,10 +12,6 @@ class Luhn:
             digits[i] = self._doubleNumber(digits[i])
 
         return sum(digits) % 10 == 0
-
-    def _guardInput(self):
-        return len(self.card) > 1 and \
-            not any(x for x in self.card if not x.isdigit())
 
     def _curateInput(self, input):
         return [int(x) for x in input if x.isdigit()]
