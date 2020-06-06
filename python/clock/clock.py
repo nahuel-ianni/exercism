a067a9df579705e086613b0040e02187d3ae0393
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-06-06 14:18:14 +0200

    Clock: Solved exercise

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,16 +1,16 @@
 class Clock:
     def __init__(self, hour, minute):
-        self.hours = hour
-        self.minutes = minute
+        self._minutes = (hour * 60 + minute) % 1440
 
     def __repr__(self):
-        return f"{self.hours:02d}:{self.minutes:02d}"
+        hours, minutes = divmod(self._minutes, 60)
+        return f'{hours:02d}:{minutes:02d}'
 
     def __eq__(self, other):
         return str(self) == str(other)
 
     def __add__(self, minutes):
-        pass
+        return self.__class__(0, self._minutes + minutes)
 
     def __sub__(self, minutes):
-        pass
+        return self.__class__(0, self._minutes - minutes)
