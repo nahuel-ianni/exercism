009a135bab348c3e41106405ef50cd46e914f043
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-07-19 11:29:58 +0200

    Pythagorean triplets: Improved solution

--- javascript/pythagorean-triplet/pythagorean-triplet.js
+++ javascript/pythagorean-triplet/pythagorean-triplet.js
@@ -1,34 +1,24 @@
 export class Triplet {
   constructor(a, b, c) {
-    this.a = a;
-    this.b = b;
-    this.c = c;
+    this.sum = () => a + b + c;
+    this.product = () => a * b * c;
+    this.isPythagorean = () => a ** 2 + b ** 2 === c ** 2 && a < b < c;
   }
 
-  sum = () => this.a + this.b + this.c;
-
-  product = () => this.a * this.b * this.c;
-
-  isPythagorean = () =>
-    this.a ** 2 + this.b ** 2 === this.c ** 2 &&
-    this.a < this.b < this.c;
-
-  static where({ maxFactor, minFactor = 1, sum = -1 }) {
+  static where({ maxFactor, minFactor = 1, sum = null }) {
     const triplets = [];
 
-    for (let i = minFactor; i <= maxFactor; ++i) {
-      for (let j = i + 1; j <= maxFactor; ++j) {
+    for (let i = minFactor; i <= maxFactor; ++i)
+      for (let j = i + 1; j <= maxFactor; ++j)
         for (let k = j + 1; k <= maxFactor; ++k) {
           const triplet = new Triplet(i, j, k);
 
-          if (sum !== -1 && triplet.sum() !== sum)
+          if (sum && triplet.sum() !== sum)
             continue;
 
           if (triplet.isPythagorean())
             triplets.push(triplet);
         }
-      }
-    }
 
     return triplets;
   }
