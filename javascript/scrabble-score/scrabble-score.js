dd7bdabc9c29bb1dc336b936c1779d68ad339b22
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-08-15 14:40:27 +0200

    Scrabble score: Solved exercise

--- javascript/scrabble-score/scrabble-score.js
+++ javascript/scrabble-score/scrabble-score.js
@@ -1,8 +1,17 @@
-//
-// This is only a SKELETON file for the 'Scrabble Score' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const scoreTable = new Map([
+  [1, ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']],
+  [2, ['D', 'G']],
+  [3, ['B', 'C', 'M', 'P']],
+  [4, ['F', 'H', 'V', 'W', 'Y']],
+  [5, ['K']],
+  [8, ['J', 'X']],
+  [10, ['Q', 'Z']]
+]);
 
-export const score = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+export const score = (word) => [...word].reduce(reducer, 0);
+
+const reducer = (accumulator, char) => {
+  const value = [...scoreTable.entries()].find(({ 1: v }) => v.includes(char.toUpperCase()))[0];
+
+  return accumulator + value;
+}
