472bf77d275700f441eafb47d6ea2189cfda0633
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-08-16 14:47:45 +0200

    Scrabble score: Updated solution

--- javascript/scrabble-score/scrabble-score.js
+++ javascript/scrabble-score/scrabble-score.js
@@ -1,17 +1,19 @@
-const scoreTable = new Map([
-  [1, ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']],
-  [2, ['D', 'G']],
-  [3, ['B', 'C', 'M', 'P']],
-  [4, ['F', 'H', 'V', 'W', 'Y']],
-  [5, ['K']],
-  [8, ['J', 'X']],
-  [10, ['Q', 'Z']]
-]);
+const scoreTable = {
+  1: ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'],
+  2: ['d', 'g'],
+  3: ['b', 'c', 'm', 'p'],
+  4: ['f', 'h', 'v', 'w', 'y'],
+  5: ['k'],
+  8: ['j', 'x'],
+  10: ['q', 'z']
+};
 
-export const score = (word) => [...word].reduce(reducer, 0);
-
-const reducer = (accumulator, char) => {
-  const value = [...scoreTable.entries()].find(({ 1: v }) => v.includes(char.toUpperCase()))[0];
-
-  return accumulator + value;
+const getScore = (char) => {
+  for (let value in scoreTable)
+    if (scoreTable[value].includes(char)) return Number(value);
 }
+
+export const score = (word) =>
+  [...word]
+    .map(char => getScore(char.toLowerCase()))
+    .reduce((sum, value) => { return sum + value }, 0);
