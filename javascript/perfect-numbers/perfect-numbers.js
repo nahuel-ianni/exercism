c2beb28b05d8270937fedee9d4a52acdab1bef92
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-10-03 16:19:13 +0200

    Perfect numbers: Reduced code

--- javascript/perfect-numbers/perfect-numbers.js
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -2,7 +2,8 @@ export const classify = (number) => {
   if (number < 1)
     throw new Error('Classification is only possible for natural numbers.');
 
-  const aliquotSum = [...Array(Math.floor(number / 2) + 1).keys()].reduce(
+  const arrayLimit = Math.floor(number / 2) + 1;
+  const aliquotSum = [...Array(arrayLimit).keys()].reduce(
     (accumulator, currentValue) =>
       number % currentValue === 0
         ? accumulator + currentValue
