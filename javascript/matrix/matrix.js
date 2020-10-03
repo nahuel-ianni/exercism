e76ab3cadc80d40be6c78d97e0153622ebe52755
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-10-03 15:20:51 +0200

    Matrix: Updated solution based on mentor feedback

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,10 +1,20 @@
 export class Matrix {
-  constructor(values) {
-    this.m_rows = values.split("\n").map(row => row.match(/\d+/g).map(Number));
-    this.m_cols = this.m_rows[0].map((_, index) => this.m_rows.map(row => row[index]));
+  m_cols;
+  m_rows;
+
+  constructor(values) { this.values = values; }
+
+  get columns() {
+    if (!this.m_cols)
+      this.m_cols = this.rows[0].map((_, index) => this.rows.map(row => row[index]));
+
+    return this.m_cols;
   }
 
-  get columns() { return this.m_cols; }
+  get rows() {
+    if (!this.m_rows)
+      this.m_rows = this.values.split("\n").map(row => row.match(/\d+/g).map(Number));
 
-  get rows() { return this.m_rows; }
+    return this.m_rows;
+  }
 }
