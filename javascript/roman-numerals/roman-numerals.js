0f9f2a8c44604d48c2a7e79756558dfdab4d8009
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-10-18 12:33:55 +0200

    Roman numerals: Solved exercise

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,13 +1,28 @@
-const conversion = {
-  1    : "I",
-  5    : "V",
-  10   : "X",
-  50   : "L",
-  100  : "C",
-  500  : "D",
-  1000 : "M",
-}
-
-export const toRoman = (num) => {
-  return conversion[num];
+const roman = {
+  'M' : 1000,
+  'CM': 900,
+  'D' : 500,
+  'CD': 400,
+  'C' : 100,
+  'XC': 90,
+  'L' : 50,
+  'XL': 40,
+  'X' : 10,
+  'IX': 9,
+  'V' : 5,
+  'IV': 4,
+  'I' : 1,
 };
+
+export const toRoman = (number) => {
+  return Object.keys(roman).map(key => {
+    let value = '';
+
+    while (number >= roman[key]) {
+      value += key;
+      number -= roman[key];
+    }
+    
+    return value;
+  }).join('');
+}
