f601d3ddcaa9ea16303b90a8688d83a0d9568663
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-10-23 08:17:41 +0200

    Beer song: Improved solution

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,16 +1,19 @@
 export const recite = (initialBottlesCount, takeDownCount = 1) => {
   const lyrics = [];
 
-  while (takeDownCount > 0) {
+  while (takeDownCount) {
     if (initialBottlesCount === 0) {
-      lyrics.push("No more bottles of beer on the wall, no more bottles of beer.");
-      lyrics.push("Go to the store and buy some more, 99 bottles of beer on the wall.");
+      lyrics.push(
+        "No more bottles of beer on the wall, no more bottles of beer.",
+        "Go to the store and buy some more, 99 bottles of beer on the wall.");
+
       break;
     }
 
     const bottles = getBottles(initialBottlesCount);
-    lyrics.push(`${bottles} of beer on the wall, ${bottles} of beer.`);
-    lyrics.push(`Take ${initialBottlesCount - 1 ? 'one' : 'it'} down and pass it around, ${getBottles(initialBottlesCount - 1)} of beer on the wall.`);
+    lyrics.push(
+      `${bottles} of beer on the wall, ${bottles} of beer.`,
+      `Take ${initialBottlesCount - 1 ? 'one' : 'it'} down and pass it around, ${getBottles(initialBottlesCount - 1)} of beer on the wall.`);
 
     takeDownCount--;
     initialBottlesCount--;
@@ -22,5 +25,4 @@ export const recite = (initialBottlesCount, takeDownCount = 1) => {
   return lyrics;
 };
 
-const getBottles = (number) =>
-  `${number ? number : 'no more'} ${number != 1 ? 'bottles' : 'bottle'}`;
+const getBottles = (number) => `${number ? number : 'no more'} ${number != 1 ? 'bottles' : 'bottle'}`;
